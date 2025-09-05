3a3f8cc16aeb26ef9f676f9fedb3dbe107b3e1a4
Billy Pesoth <billy.pesoth@gmail.com>
2025-09-06 01:03:59 +0800

    fix postgres setup

--- pkg/repository/db.go
+++ pkg/repository/db.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,22 +11,16 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASSWORD")
-	name := os.Getenv("DB_NAME")
-	ssl := os.Getenv("DB_SSLMODE")
-
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC",
-		host, user, pass, name, port, ssl,
-	)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB connect error:", err)
 	}
+
 	DB = db
-	log.Println("Database [Narwastu Connect] connected")
+	log.Println("✅ Database [Narwastu Connect] connected")
 }
